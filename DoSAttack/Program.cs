8bf40206361872494fe7e982cef463e8a37c1bbc
niquefaDiego <niquefa.diego@gmail.com>
2019-02-16 20:56:30 -0800

    refactor

--- DoSAttack/Program.cs
+++ DoSAttack/Program.cs
@@ -9,22 +9,21 @@ namespace DoSAttack
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-
+        static string request = "https://localhost:44390/?IsPalindrome=true&IsCircular=true&IsRightTruncable=true";
+        static void MakeRequest() {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add(
+                        "X-Mashape-Authorization", "1R1tvmPFLFL7brrvfO9jvsqnvhiVggAn");
+                var body = wc.DownloadString(request);
+                Console.WriteLine(body);
+            }
+        }
+        static void Main(string[] args) {
             List<Task> tasks = new List<Task>();
-            for ( int i = 0; i < 1000; ++i )
-            {
-                tasks.Add(new Task( () =>
-                {
-                    using (var wc = new WebClient())
-                    {
-                        wc.Headers.Add(
-                                "X-Mashape-Authorization", "1R1tvmPFLFL7brrvfO9jvsqnvhiVggAn");
-                        var body = wc.DownloadString("https://localhost:44390/?IsPalindrome=true&IsCircular=true&IsRightTruncable=true");
-                        Console.WriteLine(body);
-                    }
-                }));
+            for ( int i = 0; i < 1000; ++i ) {
+                Task t = new Task(() => MakeRequest());
+                t.Start();
+                tasks.Add(t);
             }
             Task.WaitAll(tasks.ToArray());
         }
